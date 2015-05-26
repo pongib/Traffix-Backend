d6c75ae0e094210ab58563aed30941f1ddd618f0
pingpong <pingpong@mail.com>
2015-05-26 22:14:20 +0700

    add new fill

--- api/busStop.js
+++ api/busStop.js
@@ -7,13 +7,15 @@ var router = express.Router();
 var BusStop = require('../traffix_model/busStop.js');
 var BusGeo = require('../traffix_model/busGeolocation');
 var BusTest = require('../traffix_model/busTest');
+var f = require('./function');
 
 
 // query all bus stop
 router.get('/all', function (req, res){
+
 	BusStop.find(function (err, busStop){
 		if(err) res.send(err);
-
+		console.log(res);
 		res.send({
 			result: busStop.length,
 			data: busStop
@@ -309,114 +311,116 @@ router.get('/linetogo/:destination', function (req, res){
 
 // find fill bus stop and estimate
 // need inbound or outbound condition 
-router.get('/findfill/:origin/:line/:distance', function (req, res){
-	var origin = req.params.origin.split(',');
-	async.waterfall([
-		function (callback){
-			BusStop.find({ line: req.params.line }).where('loc').near({
-				center: { 
-					coordinates: [parseFloat(origin[1]), parseFloat(origin[0])], 
-					type: 'Point' 
-				},
-				maxDistance: parseInt(req.params.distance),
-				spherical: true
-			}).exec(function (err, busStop){
-				if(err){
-					res.jsonp({
-						status: 'ERROR',
-						msg: 'cannot find bus stop result and ' + err
-					});
-				}
-				if(busStop.length >= 1){
-					callback(null, busStop);
-				}else {
-					res.jsonp({
-						status: 'ERROR',
-						msg: 'cannot find bus stop result'
-					});
-				}
-			});			
-		},
-		function (busStop, callback){
-			var _dest = "", temp = "", tag = [];
-			async.each(busStop, function (entry, callback){
-				//make coordinates to string and split it into array of string
-				temp = String(entry.loc.coordinates).split(',');
-				// make bus stop string and set last bus stop with no '|'
-				if(entry != busStop[busStop.length - 1]){
-					_dest += temp[1]+','+temp[0]+'|';
-				}else _dest += temp[1]+','+temp[0];
+// router.get('/findfill/:origin/:line/:distance', function (req, res){
+// 	var origin = req.params.origin.split(',');
+// 	async.waterfall([
+// 		function (callback){
+// 			BusStop.find({ line: req.params.line }).where('loc').near({
+// 				center: { 
+// 					coordinates: [parseFloat(origin[1]), parseFloat(origin[0])], 
+// 					type: 'Point' 
+// 				},
+// 				maxDistance: parseInt(req.params.distance),
+// 				spherical: true
+// 			}).exec(function (err, busStop){
+// 				if(err){
+// 					res.jsonp({
+// 						status: 'ERROR',
+// 						msg: 'cannot find bus stop result and ' + err
+// 					});
+// 				}
+// 				if(busStop.length >= 1){
+// 					callback(null, busStop);
+// 				}else {
+// 					res.jsonp({
+// 						status: 'ERROR',
+// 						msg: 'cannot find bus stop result'
+// 					});
+// 				}
+// 			});			
+// 		},
+// 		function (busStop, callback){
+// 			var _dest = "", temp = "", tag = [];
+// 			async.each(busStop, function (entry, callback){
+// 				//make coordinates to string and split it into array of string
+// 				temp = String(entry.loc.coordinates).split(',');
+// 				// make bus stop string and set last bus stop with no '|'
+// 				if(entry != busStop[busStop.length - 1]){
+// 					_dest += temp[1]+','+temp[0]+'|';
+// 				}else _dest += temp[1]+','+temp[0];
 
-				if(entry.tag){
-					tag.push(entry.tag);
-				}
+// 				if(entry.tag){
+// 					tag.push(entry.tag);
+// 				}
 
 					
-				callback();
-				// console.log(_dest);
-			}, function (err) {			
-				callback(null, _dest, tag);
-			});
-		}
-		,function (busStop, tags, callback){
-			console.log(tags);
-			var busStopJson = []; 
-			BusGeo.find().and([{ line: req.params.line }, { tag : { $nin : tags }}])
-			.where('accuracy').lte(10).sort({accuracy: 'asc'}).limit(1)
-			.exec(function (err, first){
-				if(err) {
-					res.jsonp({
-						status: 'ERROR',
-						msg: 'cannot find bus geo result and ' + err
-					});
-				}
-				if(first.length >= 1){
-					var currentBus = first[0].loc.coordinates[1]+','+first[0].loc.coordinates[0];
-					console.log("currentBus = "+currentBus+" busStop = "+busStop);
+// 				callback();
+// 				// console.log(_dest);
+// 			}, function (err) {			
+// 				callback(null, _dest, tag);
+// 			});
+// 		}
+// 		,function (busStop, tags, callback){
+// 			console.log(tags);
+// 			var busStopJson = []; 
+// 			BusGeo.find().and([{ line: req.params.line }, { tag : { $nin : tags }}])
+// 			.where('accuracy').lte(10).sort({accuracy: 'asc'}).limit(1)
+// 			.exec(function (err, first){
+// 				if(err) {
+// 					res.jsonp({
+// 						status: 'ERROR',
+// 						msg: 'cannot find bus geo result and ' + err
+// 					});
+// 				}
+// 				if(first.length >= 1){
+// 					var currentBus = first[0].loc.coordinates[1]+','+first[0].loc.coordinates[0];
+// 					console.log("currentBus = "+currentBus+" busStop = "+busStop);
 
-					busStop.split('|').forEach(function (entry){
-						busStopJson.push(JSON.parse('{ "lat": '+entry.split(',')[0]+', "lng": '+entry.split(',')[1]+'}'));
-					});
-					// res.send(result);	
-					gm.distance(
-					currentBus, //origin
-					busStop,  //destination
-					function (err, estimate){
-					  if(err) {
-					  	res.jsonp({
-							status: 'ERROR',
-							msg: 'cannot find bus stop result and ' + err
-						});				
-					  }
-					  // res.send(estimate); 
-					  if(estimate){
-					  	if(estimate.status == "OK"){
-						  	var _estimate = {
-							  	status: estimate.status,
-							  	line: parseInt(req.params.line),
-							  	origin: currentBus,
-							  	busstop: busStopJson,					  	
-							  	estimate: estimate.rows[0].elements
-						  	};
-							callback(null, _estimate);
-						 }else res.jsonp(estimate);		
-					  }else {
-					  	res.jsonp({
-							status: 'ERROR',
-							msg: 'cannot estimate the time'
-						});		
-					  }
-					}, false, "transit");
-				}				
-			});
-		}
-	], 	function (err, results){
-		if (err) res.jsonp(err);
+// 					busStop.split('|').forEach(function (entry){
+// 						busStopJson.push(JSON.parse('{ "lat": '+entry.split(',')[0]+', "lng": '+entry.split(',')[1]+'}'));
+// 					});
+// 					// res.send(result);	
+// 					gm.distance(
+// 					currentBus, //origin
+// 					busStop,  //destination
+// 					function (err, estimate){
+// 					  if(err) {
+// 					  	res.jsonp({
+// 							status: 'ERROR',
+// 							msg: 'cannot find bus stop result and ' + err
+// 						});				
+// 					  }
+// 					  // res.send(estimate); 
+// 					  if(estimate){
+// 					  	if(estimate.status == "OK"){
+// 						  	var _estimate = {
+// 							  	status: estimate.status,
+// 							  	line: parseInt(req.params.line),
+// 							  	origin: currentBus,
+// 							  	busstop: busStopJson,					  	
+// 							  	estimate: estimate.rows[0].elements
+// 						  	};
+// 							callback(null, _estimate);
+// 						 }else res.jsonp(estimate);		
+// 					  }else {
+// 					  	res.jsonp({
+// 							status: 'ERROR',
+// 							msg: 'cannot estimate the time'
+// 						});		
+// 					  }
+// 					}, false, "transit");
+// 				}				
+// 			});
+// 		}
+// 	], 	function (err, results){
+// 		if (err) res.jsonp(err);
 		
-		res.jsonp(results);
-	});
-});
+// 		res.jsonp(results);
+// 	});
+// });
 
+// new fill 
+router.get('/findfill/:origin/:line/:distance', f.findFill);
 
 
 // find nearest bus stop
